0547425e7984432161838875dfd48b630cbddd14
AM021701 <mag840823@gmail.com>
2015-04-27 10:43:59 +0800

    A AM021701 林倪君提交

--- Question3.java
+++ Question3.java
@@ -5,3 +5,12 @@ public class Question3{
 	}
 
 }
+
+class ThreadA extends Thread{
+	public void run(){
+		for(int i=0 ; i<0010 ;i++){
+			System.out.println("ThreadA");
+			try{this.sleep(1000);}catch(Exception e){}
+		}
+	}
+}
