22ec4492f946165767a6fa13c80647a95efee0ce
AM021701 <mag840823@gmail.com>
2015-04-27 10:51:47 +0800

    A AM021701 林倪君提交

--- Question3.java
+++ Question3.java
@@ -8,7 +8,7 @@ public class Question3{
 
 class ThreadA extends Thread{
 	public void run(){
-		for(int i=0 ; i<0010 ;i++){
+		for(int i=0 ; i<1000 ;i++){
 			System.out.println("ThreadA");
 			try{this.sleep(1000);}catch(Exception e){}
 		}
